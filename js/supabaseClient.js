7a4afd6d2587ebf71797cb76630425a7909cd2ce
VICTOR LEME COSTA <88389939+VictorLemeCosta@users.noreply.github.com>
2026-07-23 12:10:16 -0300

    Add files via upload

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,8 @@
-const SUPABASE_URL = "https://yulxzbgudbmzedijndmd.supabase.co";
+window.SUPABASE_URL = "https://yulxzbgudbmzedijndmd.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_Z9ul2ddpAn0QCGkkXMFr-w_-MJouFhc";
+window.SUPABASE_ANON_KEY = "sb_publishable_Z9ul2ddpAn0QCGkkXMFr-w_-MJouFhc";
 
 window.supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  window.SUPABASE_URL,
+  window.SUPABASE_ANON_KEY
 );
